65abb93e5f411b7db49f312db585862625ffc3d5
kimgb415 <kimgb415@icloud.com>
2020-05-14 10:51:59 +0900

    2020-05-14 version

--- App.tsx
+++ App.tsx
@@ -1,19 +1,81 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import "react-native-gesture-handler";
+import * as React from "react";
+import { Component } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BasicInfo from "./pages/basicInfo";
+import FoodSelect from "./pages/foodSelect";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Map from "./component/map";
+import Main from "./pages/main";
+import FoodRecommend from "./pages/foodRecommend";
+import Recipe from "./pages/recipe";
+import OnlyOneFood from "./pages/onlyOneFood";
+import UserIdProvider from "./context/UserId";
+import Login from "./pages/login";
+import CountProvider, { Count } from "./context/FoodSelectCount";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+const Stack = createStackNavigator();
+
+export default class App extends Component {
+  render() {
+    return (
+      <UserIdProvider>
+        <CountProvider>
+          <NavigationContainer>
+            <Stack.Navigator>
+              <Stack.Screen
+                name="Login"
+                component={Login}
+                options={{ gestureEnabled: false }}
+              />
+              <Stack.Screen
+                name="BasicInfo"
+                component={BasicInfo}
+                options={{
+                  gestureEnabled: false,
+                  headerLeft: () => {
+                    <View></View>;
+                  },
+                }}
+              />
+              <Stack.Screen
+                name="FoodSelect"
+                component={FoodSelect}
+                options={{
+                  gestureEnabled: false,
+                  headerLeft: () => {
+                    <View></View>;
+                  },
+                }}
+              />
+              <Stack.Screen
+                name="Main"
+                component={Main}
+                options={{
+                  gestureEnabled: false,
+                  headerLeft: () => {
+                    <View></View>;
+                  },
+                }}
+              />
+              <Stack.Screen name="FoodRecommend" component={FoodRecommend} />
+              <Stack.Screen name="OnlyOneFood" component={OnlyOneFood} />
+              <Stack.Screen name="Map" component={Map} />
+              <Stack.Screen name="Recipe" component={Recipe} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </CountProvider>
+      </UserIdProvider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
